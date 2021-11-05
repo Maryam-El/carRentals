ad9cc9aa76628facede146ae370ee7a96aa8e987
maryamEl <maryamelarfaoui5@gmail.com>
2021-11-05 18:53:35 -0400

    remove target folders

--- car-rentals-persistence/target/classes/populateDb/V001_0002_0__create_table_client.sql
+++
@@ -1,8 +0,0 @@
--- /*==============================================================*/
--- /* Table : client                                              */
--- /*==============================================================*/
-CREATE TABLE client (
-  id_client VARCHAR(255)  PRIMARY KEY,
-  first_name_client VARCHAR(255) NOT NULL,
-  last_name_client VARCHAR(255) NOT NULL
-);
--- car-rentals-persistence/target/classes/populateDb/V001_0004_0__insert_in_client_table.sql
+++
@@ -1,9 +0,0 @@
-insert into client values ('Client1','Jean','charbonneau'),
-('Client2','Marie','Latendresse'),
-('Client3','Ahmed','raghib'),
-('Client4','Yassine','arif'),
-('Client5','Henri','bourassa');
-
-
-
-
